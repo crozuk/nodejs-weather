d7d4f2908353c39311ed4065b67ff9798456c19a
crozuk <crozuk@gmail.com>
2019-06-10 01:37:31 +0000

    Initial commit

---
+++ weather.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const apiKey = process.env.api_key;
+
+const bodyParser = require('body-parser');
+const request = require('request');
+
+module.exports = {
+getWeather :    
+    function getWeather(input, callback){
+        //var input = "Bristol";
+        let city = input;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    
+        request(url, function (err, response, body) {
+            if(err){
+              console.log('Error, please try again'); 
+            } else {
+              let weather = JSON.parse(body)
+              if(weather.main == undefined){
+                console.log('Error, please try again');
+              } else {
+                let data = weather;
+                return callback(data);
+              }
+            }
+        });
+    }
+}
